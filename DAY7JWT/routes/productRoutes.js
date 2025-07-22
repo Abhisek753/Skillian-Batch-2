55f2c7dc48ac4c976b1b4647da02a90e18d84822
Abhisek753 <properachievement@gmail.com>
2025-07-22 11:41:19 +0530

    jwt auth

---
+++ DAY7JWT/routes/productRoutes.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const { getAllProducts,addNewProduct,updateProduct,deleteProduct} = require("../controllers/productController");
+
+const router=express.Router();
+
+router.get("/getall",getAllProducts);
+router.post("/add-product",addNewProduct);
+router.put("/update-product/:id",updateProduct);
+router.delete("/delete-product/:id",deleteProduct);
+
+
+module.exports=router;
