5df57872013bd113a71d10953f72090be747cde3
Abhisek753 <properachievement@gmail.com>
2025-08-26 20:27:18 +0530

    added product and single product

--- E-Commerce/controllers/productController.js
+++ E-Commerce/controllers/productController.js
@@ -67,7 +67,16 @@ exports.getProducts=async (req,res)=>{
   
 };
 exports.getProductById=async (req,res)=>{
-    
+    try{
+      const product= await Product.findById(req.params.id);
+      if(!product){
+        return   res.status(404).json({message:"Product not found"})
+      }
+      res.status(200).json({message:"Got Single Product",product})
+
+    }catch(err){
+      res.status(500).json({message:"Error in getting the product"})
+    }
   
 };
 exports.updateProduct=async (req,res)=>{
